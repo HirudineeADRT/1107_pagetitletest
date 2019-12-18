1006cc17d0f4eae3a7f277c910ab95f0a151f017
HirudineeADRT <hirudinee@adroitlogic.com>
2019-12-18 13:19:56 +0530

    changed the code

--- 1107_pagetitletest/lambda.js
+++ 1107_pagetitletest/lambda.js
@@ -1,4 +1,46 @@
-exports.handler = function(event, context, callback) {
-    
-    callback(null, {"message": "Successfully executed //"});
+let AWS = require('aws-sdk');
+let SL_AWS = require('slappforge-sdk-aws');
+const sqs = new SL_AWS.SQS(AWS);
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = function (event, context, callback) {
+    ddb.get({
+        TableName: "jjl",
+        Key: {
+            kk: "ll"
+        }
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            // error handling goes here
+        });
+
+    sqs.receiveMessage({
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/hirru`,
+        AttributeNames: ['All'],
+        MaxNumberOfMessages: '1',
+        VisibilityTimeout: '30',
+        WaitTimeSeconds: '0'
+    }).promise()
+        .then(receivedMsgData => {
+            if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
+                let receivedMessages = receivedMsgData.Messages;
+                receivedMessages.forEach(message => {
+                    // your logic to access each message through out the loop. Each message is available under variable message 
+                    // within this block
+                });
+            } else {
+                // No messages to process
+            }
+        })
+        .catch(err => {
+            // error handling goes here
+        });
+
+
+    callback(null, { "message": "Successfully executed //" });
+
+
 }
